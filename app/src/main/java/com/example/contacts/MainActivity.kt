e77168c0662c00ce6faaaad0e8a04de60328cd49
Student406-11 <Student406-11@NPK-102O-2>
2025-09-04 13:41:39 +0700

    1

--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -11,11 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.contacts.databinding.ActivityMainBinding
 import com.example.contacts.ui.theme.ContactsTheme
 
 class MainActivity : ComponentActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-
+        setContentView(binding.root)
     }
 }
